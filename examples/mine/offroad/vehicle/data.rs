7b0792fde8b0e6ff4ee6bf2231d8c82e4ea536b6
Pierre Gueth <gueth@adobe.com>
2025-03-08 14:23:21 +0100

    fsdggdf

---
+++ examples/mine/offroad/vehicle/data.rs
@@ -0,0 +1,125 @@
+use bevy::math::{ops, Vec2};
+use bevy::prelude::Component;
+
+use std::collections::HashMap;
+use std::fmt::{Display, Formatter, Result};
+use std::time::Duration;
+
+#[derive(Clone, PartialEq)]
+pub enum Player {
+    One,
+    Two,
+    Three,
+}
+
+impl Display for Player {
+    fn fmt(&self, buffer: &mut Formatter) -> Result {
+        match self {
+            Player::One => write!(buffer, "P1"),
+            Player::Two => write!(buffer, "P2"),
+            Player::Three => write!(buffer, "P3"),
+        }
+    }
+}
+
+#[derive(Clone, PartialEq)]
+pub struct LapStat {
+    top_start: Duration,
+    checkpoint_to_tops: HashMap<u8, Duration>,
+    top_finish: Duration,
+}
+
+impl LapStat {
+    pub fn invalid() -> Self {
+        Self {
+            top_start: Duration::MAX,
+            checkpoint_to_tops: HashMap::new(),
+            top_finish: Duration::MAX,
+        }
+    }
+
+    pub fn update(&mut self, top: Duration) {
+        self.top_finish = top;
+    }
+
+    pub fn patate(&mut self, checkpoint: u8, checkpoint_count: u8, top: Duration) -> bool {
+        // assert!(self.top_start != Duration::MAX);
+        assert!(self.top_finish != Duration::MAX);
+        // assert!(self.top_start <= self.top_finish);
+        if checkpoint == 0 {
+            if self.top_start == Duration::MAX {
+                self.top_start = top;
+                false
+            } else {
+                let mut crossed_all = true;
+                for kk in 1..checkpoint_count {
+                    crossed_all &= self.checkpoint_to_tops.contains_key(&kk);
+                }
+                crossed_all
+            }
+        } else {
+            self.checkpoint_to_tops.insert(checkpoint, top);
+            false
+        }
+    }
+
+    pub fn is_valid(&self) -> bool {
+        if self.top_start == Duration::MAX {
+            return false;
+        }
+        if self.top_finish == Duration::MAX {
+            return false;
+        }
+        if self.top_start <= self.top_finish {
+            return false;
+        }
+        true
+    }
+
+    pub fn lap_duration(&self) -> Duration {
+        assert!(self.top_start != Duration::MAX);
+        assert!(self.top_finish != Duration::MAX);
+        assert!(self.top_start <= self.top_finish);
+        self.top_finish - self.top_start
+    }
+}
+
+#[derive(Component, Clone)]
+pub struct BoatData {
+    pub player: Player,
+    position_initial: Vec2,
+    pub position_previous: Vec2,
+    pub position_current: Vec2,
+    angle_initial: f32,
+    pub angle_current: f32,
+    pub current_stat: LapStat,
+    pub maybe_last_stat: Option<LapStat>,
+    pub maybe_best_stat: Option<LapStat>,
+    pub lap_count: u32,
+}
+
+impl BoatData {
+    pub fn from_player_position_and_forward(player: Player, pos: Vec2, fwd: Vec2) -> Self {
+        let angle = ops::atan2(fwd.x, fwd.y); // FIXME should be the other way around
+        Self {
+            player,
+            position_initial: pos,
+            position_previous: pos,
+            position_current: pos,
+            angle_initial: angle,
+            angle_current: angle,
+            current_stat: LapStat::invalid(),
+            maybe_last_stat: None,
+            maybe_best_stat: None,
+            lap_count: 0,
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.position_previous = self.position_initial;
+        self.position_current = self.position_initial;
+        self.angle_current = self.angle_initial;
+        self.current_stat = LapStat::invalid();
+        self.lap_count = 0;
+    }
+}
--- examples/mine/offroad/vehicle/mod.rs
+++ examples/mine/offroad/vehicle/mod.rs
@@ -1,11 +1,17 @@
+mod data;
+mod physics;
+
+use data::BoatData;
+use data::LapStat;
+use data::Player;
+
 use crate::global_state::{GlobalState, TrackNickname, TRACK_NICKNAMES};
 use crate::material::racing_line_material;
 use crate::track::{Segment, Track, TRACK_HANDLES};
 
 use bevy::asset::{AssetServer, Assets};
 use bevy::color::Srgba;
-use bevy::math::ops;
-use bevy::math::{Mat2, Quat, Vec2, Vec3, Vec3Swizzles};
+use bevy::math::Vec3Swizzles;
 use bevy::pbr::{NotShadowCaster, StandardMaterial};
 
 use bevy::prelude::MeshMaterial3d;
@@ -15,11 +21,7 @@ use bevy::prelude::{info, warn};
 use bevy::prelude::{ButtonInput, KeyCode};
 use bevy::prelude::{Commands, Component, NextState, Query, Res, ResMut, With};
 use bevy::prelude::{Entity, Time, Transform};
-use bevy::prelude::{Gamepad, GamepadAxis, GamepadButton};
-
-use std::collections::HashMap;
-use std::fmt;
-use std::time::Duration;
+// use bevy::prelude::{Gamepad, GamepadAxis, GamepadButton};
 
 const COLOR_P1: Srgba = bevy::color::palettes::css::LIGHT_GRAY;
 const COLOR_P2: Srgba = bevy::color::palettes::css::HOT_PINK;
@@ -38,8 +40,6 @@ const COLOR_SECOND: Srgba = bevy::color::palettes::css::SILVER;
 const COLOR_THIRD: Srgba = bevy::color::palettes::css::ROSY_BROWN;
 const COLOR_CUP: Srgba = bevy::color::palettes::css::BLACK;
 
-use std::f32::consts::PI;
-
 //////////////////////////////////////////////////////////////////////
 
 pub struct VehiclePlugin;
@@ -55,7 +55,7 @@ impl bevy::prelude::Plugin for VehiclePlugin {
                 Update,
                 (
                     reset_vehicle_positions,
-                    update_vehicle_physics,
+                    physics::update_vehicle_physics,
                     resolve_checkpoints,
                     update_boards,
                     exit_game,
@@ -64,95 +64,6 @@ impl bevy::prelude::Plugin for VehiclePlugin {
                     .run_if(in_state(state)),
             );
         }
-    }
-}
-
-//////////////////////////////////////////////////////////////////////
-
-#[derive(Clone, PartialEq)]
-enum Player {
-    One,
-    Two,
-    Three,
-}
-
-impl fmt::Display for Player {
-    fn fmt(&self, buffer: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Player::One => write!(buffer, "P1"),
-            Player::Two => write!(buffer, "P2"),
-            Player::Three => write!(buffer, "P3"),
-        }
-    }
-}
-
-#[derive(Clone, PartialEq)]
-struct LapStat {
-    top_start: Duration,
-    checkpoint_to_tops: HashMap<u8, Duration>,
-    top_finish: Duration,
-}
-
-impl LapStat {
-    fn from(top: Duration) -> Self {
-        Self {
-            top_start: top,
-            checkpoint_to_tops: HashMap::new(),
-            top_finish: top,
-        }
-    }
-
-    fn elapsed_secs(&self) -> f32 {
-        if self.top_start == Duration::MAX || self.top_finish == Duration::MAX {
-            return 0.0;
-        }
-        if self.top_start == self.top_finish {
-            return 0.0;
-        }
-        assert!(self.top_start != Duration::MAX);
-        assert!(self.top_finish != Duration::MAX);
-        assert!(self.top_start < self.top_finish);
-        (self.top_finish - self.top_start).as_secs_f32()
-    }
-}
-
-#[derive(Component, Clone)]
-struct BoatData {
-    player: Player,
-    position_initial: Vec2,
-    position_previous: Vec2,
-    position_current: Vec2,
-    angle_initial: f32,
-    angle_current: f32,
-    current_stat: LapStat,
-    maybe_last_stat: Option<LapStat>,
-    maybe_best_stat: Option<LapStat>,
-    lap_count: u32,
-}
-
-impl BoatData {
-    fn from_player_position_and_forward(player: Player, pos: Vec3, fwd: Vec3) -> Self {
-        let angle = ops::atan2(fwd.x, fwd.z);
-        let pos = pos.xz();
-        Self {
-            player,
-            position_initial: pos,
-            position_previous: pos,
-            position_current: pos,
-            angle_initial: angle,
-            angle_current: angle,
-            current_stat: LapStat::from(Duration::MAX),
-            maybe_last_stat: None,
-            maybe_best_stat: None,
-            lap_count: 0,
-        }
-    }
-    fn reset(&mut self) {
-        self.position_previous = self.position_initial;
-        self.position_current = self.position_initial;
-        self.angle_current = self.angle_initial;
-        self.current_stat = LapStat::from(Duration::MAX);
-        self.lap_count = 0;
     }
 }
 
@@ -280,19 +191,31 @@ fn populate_vehicles(
     commands.spawn((
         VehicleSceneMarker,
         SceneRoot(model_p1),
-        BoatData::from_player_position_and_forward(Player::One, pos_p1, track.initial_forward),
+        BoatData::from_player_position_and_forward(
+            Player::One,
+            pos_p1.xz(),
+            track.initial_forward.xz(),
+        ),
         Transform::from_scale(Vec3::ONE * MODEL_SCALE),
     ));
     commands.spawn((
         VehicleSceneMarker,
         SceneRoot(model_p2),
-        BoatData::from_player_position_and_forward(Player::Two, pos_p2, track.initial_forward),
+        BoatData::from_player_position_and_forward(
+            Player::Two,
+            pos_p2.xz(),
+            track.initial_forward.xz(),
+        ),
         Transform::from_scale(Vec3::ONE * MODEL_SCALE),
     ));
     commands.spawn((
         VehicleSceneMarker,
         SceneRoot(model_p3),
-        BoatData::from_player_position_and_forward(Player::Three, pos_p3, track.initial_forward),
+        BoatData::from_player_position_and_forward(
+            Player::Three,
+            pos_p3.xz(),
+            track.initial_forward.xz(),
+        ),
         Transform::from_scale(Vec3::ONE * MODEL_SCALE),
     ));
 
@@ -379,6 +302,8 @@ fn update_boards(
     tracks: Res<Assets<Track>>,
     state: Res<State<GlobalState>>,
 ) {
+    use std::time::Duration;
+
     let track = match state.get() {
         GlobalState::InGame(TrackNickname::Beginner) => tracks.get(&TRACK_HANDLES[0]),
         GlobalState::InGame(TrackNickname::Vertical) => tracks.get(&TRACK_HANDLES[1]),
@@ -395,10 +320,7 @@ fn update_boards(
         let Some(best_stat) = &boat.maybe_best_stat else {
             continue;
         };
-        assert!(best_stat.top_start != Duration::MAX);
-        assert!(best_stat.top_finish != Duration::MAX);
-        assert!(best_stat.top_start < best_stat.top_finish);
-        let lap_duration = best_stat.top_finish - best_stat.top_start;
+        let lap_duration = best_stat.lap_duration();
         sorted_lap_duration_boats.push((lap_duration, boat));
     }
     sorted_lap_duration_boats.sort_by_key(|(lap_duration, _)| *lap_duration);
@@ -472,7 +394,7 @@ fn resolve_checkpoints(
         let closest_segment = track.track_kdtree.nearest(&query_segment).unwrap();
         assert!(query_segment.ii == 255);
         assert!(closest_segment.item.ii == 0 || closest_segment.item.ii == 1);
-        if Segment::clips(closest_segment.item, &query_segment) {
+        if closest_segment.item.clips(&query_segment) {
             boat.position_previous = closest_segment.item.mirror(boat.position_previous);
             boat.position_current = closest_segment.item.mirror(boat.position_current);
         }
@@ -481,50 +403,38 @@ fn resolve_checkpoints(
     // update crossed checkpoints
     let top_now = time.elapsed();
     for mut boat in &mut boats {
-        boat.current_stat.top_finish = top_now;
         let query_segment = Segment::from_endpoints(boat.position_current, boat.position_previous);
         let closest_segment = track.checkpoint_kdtree.nearest(&query_segment).unwrap();
         assert!(query_segment.ii == 255);
         assert!(closest_segment.item.ii != 255);
+        boat.current_stat.update(top_now);
         if closest_segment.item.intersects(&query_segment) {
-            if closest_segment.item.ii == 0 {
-                if boat.current_stat.top_start == Duration::MAX {
-                    boat.current_stat.top_start = top_now;
-                } else {
-                    let mut crossed_all_checkpoints = true;
-                    for kk in 1..track.checkpoint_count {
-                        crossed_all_checkpoints &=
-                            boat.current_stat.checkpoint_to_tops.contains_key(&kk);
+            if boat
+                .current_stat
+                .patate(closest_segment.item.ii, track.checkpoint_count, top_now)
+            {
+                boat.maybe_last_stat = Some(boat.current_stat.clone());
+                boat.maybe_best_stat = Some(match &boat.maybe_best_stat {
+                    None => boat.current_stat.clone(),
+                    Some(best_stat) => {
+                        if boat.current_stat.lap_duration() < best_stat.lap_duration() {
+                            boat.current_stat.clone()
+                        } else {
+                            best_stat.clone()
+                        }
                     }
-                    if crossed_all_checkpoints {
-                        boat.maybe_last_stat = Some(boat.current_stat.clone());
-                        boat.maybe_best_stat = Some(match &boat.maybe_best_stat {
-                            None => boat.current_stat.clone(),
-                            Some(best_stat) => {
-                                if boat.current_stat.elapsed_secs() < best_stat.elapsed_secs() {
-                                    boat.current_stat.clone()
-                                } else {
-                                    best_stat.clone()
-                                }
-                            }
-                        });
-                        boat.lap_count += 1;
-                        let is_new_best: bool =
-                            boat.maybe_best_stat.clone() == boat.maybe_last_stat.clone();
-                        warn!(
-                            "player {} completed lap {} in {:>6.3}{}",
-                            boat.player,
-                            boat.lap_count,
-                            boat.current_stat.elapsed_secs(),
-                            if is_new_best { " NEW BEST LAP !!!" } else { "" },
-                        );
-                        boat.current_stat = LapStat::from(top_now);
-                    }
-                }
-            } else {
-                boat.current_stat
-                    .checkpoint_to_tops
-                    .insert(closest_segment.item.ii, top_now);
+                });
+                boat.lap_count += 1;
+                let is_new_best: bool =
+                    boat.maybe_best_stat.clone() == boat.maybe_last_stat.clone();
+                warn!(
+                    "player {} completed lap {} in {:>6.3}{}",
+                    boat.player,
+                    boat.lap_count,
+                    boat.current_stat.lap_duration().as_secs_f32(),
+                    if is_new_best { " NEW BEST LAP !!!" } else { "" },
+                );
+                boat.current_stat = LapStat::invalid();
             }
         }
     }
@@ -536,17 +446,21 @@ fn resolve_checkpoints(
             "{} lap{}\ncurrent   last   best\n{:>6.3} {:>6.3} {:>6.3}",
             boat.player,
             boat.lap_count,
-            boat.current_stat.elapsed_secs(),
+            match boat.current_stat.is_valid() {
+                true => boat.current_stat.lap_duration().as_secs_f32(),
+                false => -1.0,
+            },
             match &boat.maybe_last_stat {
                 None => 0.0,
-                Some(stat) => stat.elapsed_secs(),
+                Some(stat) => stat.lap_duration().as_secs_f32(),
             },
             match &boat.maybe_best_stat {
                 None => 0.0,
-                Some(best_stat) => best_stat.elapsed_secs(),
+                Some(stat) => stat.lap_duration().as_secs_f32(),
             },
         ));
 
+        /*
         for kk in 1..track.checkpoint_count {
             let maybe_checkpoint_top = boat.current_stat.checkpoint_to_tops.get(&kk);
             let aa: String = match maybe_checkpoint_top {
@@ -593,111 +507,8 @@ fn resolve_checkpoints(
             };
             ss.push(format!("#{} {} {} {}", kk, aa, bb, cc));
         }
+        */
 
         *status_label = ss.join("\n").into();
-    }
-}
-
-struct BoatPhysics {
-    mass: f32,
-    friction: Vec2,
-    thrust: f32,
-    brake: f32,
-    turning_speed: f32,
-    force: Vec2,
-    dt: f32,
-}
-
-impl BoatPhysics {
-    fn from_dt(dt: f32) -> Self {
-        Self {
-            mass: 100.0,                     // kg
-            friction: Vec2::new(5e-2, 1e-2), // 0 <= f < 1
-            thrust: 1500.0,                  // m / s^2 / kg ~ N
-            brake: 800.0,                    // m / s^2 / kg ~ N
-            turning_speed: 5.0 * PI / 4.0,   // rad / s
-            force: Vec2::ZERO,               // m / s^2 /kg ~ N
-            dt,                              // s
-        }
-    }
-}
-
-impl BoatPhysics {
-    fn compute_next_pos(&self, pos_prev: Vec2, pos_current: Vec2, angle_current: f32) -> Vec2 {
-        let accel = self.force / self.mass / 2.0;
-        let pp = Mat2::from_angle(angle_current);
-        let friction = pp.transpose() * Mat2::from_diagonal(self.friction) * pp;
-        (2.0 * Mat2::IDENTITY - friction) * pos_current
-            - (1.0 * Mat2::IDENTITY - friction) * pos_prev
-            + accel * self.dt * self.dt
-    }
-}
-
-fn update_vehicle_physics(
-    mut boats: Query<(&mut BoatData, &mut Transform)>,
-    time: Res<Time>,
-    keyboard: Res<ButtonInput<KeyCode>>,
-    gamepads: Query<(Entity, &Gamepad)>,
-) {
-    let dt = time.delta_secs();
-    for (mut boat, mut transform) in &mut boats {
-        let pos_prev = boat.position_previous;
-        let pos_current = boat.position_current;
-        let mut physics = BoatPhysics::from_dt(dt);
-        match boat.player {
-            Player::One => {
-                if keyboard.pressed(KeyCode::ArrowLeft) {
-                    boat.angle_current += physics.turning_speed * dt;
-                }
-                if keyboard.pressed(KeyCode::ArrowRight) {
-                    boat.angle_current -= physics.turning_speed * dt;
-                }
-                let dir_current = Vec2::from_angle(PI / 2.0 - boat.angle_current);
-                if keyboard.pressed(KeyCode::ArrowUp) {
-                    physics.force += physics.thrust * dir_current;
-                }
-                if keyboard.pressed(KeyCode::ArrowDown) {
-                    // physics.friction = Vec2::ONE * 0.10;
-                    physics.force -= physics.brake * dir_current;
-                }
-            }
-            Player::Three => {
-                if keyboard.pressed(KeyCode::KeyA) {
-                    boat.angle_current += physics.turning_speed * dt;
-                }
-                if keyboard.pressed(KeyCode::KeyD) {
-                    boat.angle_current -= physics.turning_speed * dt;
-                }
-                let dir_current = Vec2::from_angle(PI / 2.0 - boat.angle_current);
-                if keyboard.pressed(KeyCode::KeyW) {
-                    physics.force += physics.thrust * dir_current;
-                }
-                if keyboard.pressed(KeyCode::KeyS) {
-                    // physics.friction = Vec2::ONE * 0.10;
-                    physics.force -= physics.brake * dir_current;
-                }
-            }
-            Player::Two => {
-                for (_, gamepad) in &gamepads {
-                    let left_stick_x = gamepad.get(GamepadAxis::LeftStickX).unwrap();
-                    if left_stick_x.abs() > 0.01 {
-                        boat.angle_current -= physics.turning_speed * left_stick_x * dt;
-                    }
-                    let dir_current = Vec2::from_angle(PI / 2.0 - boat.angle_current);
-                    if gamepad.pressed(GamepadButton::East) {
-                        physics.force += physics.thrust * dir_current;
-                    }
-                    if gamepad.pressed(GamepadButton::North) {
-                        // physics.friction = Vec2::ONE * 0.10;
-                        physics.force -= physics.brake * dir_current;
-                    }
-                }
-            }
-        };
-        let pos_next = physics.compute_next_pos(pos_prev, pos_current, boat.angle_current);
-        boat.position_previous = boat.position_current;
-        boat.position_current = pos_next;
-        transform.translation = Vec3::new(pos_next.x, 0.0, pos_next.y);
-        transform.rotation = Quat::from_axis_angle(Vec3::Y, boat.angle_current);
     }
 }
